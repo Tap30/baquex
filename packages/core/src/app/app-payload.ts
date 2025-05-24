6d7b5d327e23a06fe9947b0d75c2c2e4a48a8538
mimshins <mostafa.sh.coderino@gmail.com>
2025-05-24 18:36:50 +0330

    chore(core): remove redundancies from app

--- packages/core/src/app/app-payload.ts
+++
@@ -1,7 +0,0 @@
-import appPayload from "./app-payload.json" with { type: "json" };
-
-type AppPayload = {
-  mainEntry: string;
-};
-
-export const APP_PAYLOAD: AppPayload = appPayload;
--- packages/core/src/app/main.tsx
+++
@@ -1,3 +0,0 @@
-import { APP_PAYLOAD } from "./app-payload.ts";
-
-await import(APP_PAYLOAD.mainEntry);
